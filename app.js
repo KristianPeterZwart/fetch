7bab81fcd8c306d6e5f17253c181a89899323f7a
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-03-08 14:50:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-fetch("https://api.adviceslip.com/advice")
+
+
+const button = document.querySelector("button");
+const header = document.querySelector("h1");
+
+button.addEventListener("click",() =>{
+    fetch("https://api.adviceslip.com/advice")
 .then(result => result.json())
-.then(data => console.log(data));
-
-
+.then(data => {
+    console.log(data.slip.advice); 
+});
+});
